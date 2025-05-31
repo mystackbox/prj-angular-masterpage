79b55a4660379479c3d6daa39d5da875382298c3
Yingi <yingishirinda@gmail.com>
2025-06-01 01:13:52 +0200

    auto-generated components

---
+++ src/app/layout/masterpage/masterpage.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-masterpage',
+  imports: [],
+  templateUrl: './masterpage.component.html',
+  styleUrl: './masterpage.component.scss'
+})
+export class MasterpageComponent {
+
+}
